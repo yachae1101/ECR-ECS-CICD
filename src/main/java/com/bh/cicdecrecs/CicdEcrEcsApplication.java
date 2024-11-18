8165b265c359b9c5b8cad7bfce012478ee7fc7ea
yachae1101 <yachae1101@naver.com>
2024-11-18 09:26:21 +0900

    init

---
+++ src/main/java/com/bh/cicdecrecs/CicdEcrEcsApplication.java
@@ -0,0 +1,13 @@
+package com.bh.cicdecrecs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdEcrEcsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdEcrEcsApplication.class, args);
+    }
+
+}
